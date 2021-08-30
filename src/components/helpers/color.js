324d7f7cc5c3b50324e5d579b56625bfe1567d95
dahee kim <ekekekgml@naver.com>
2021-08-30 09:33:38 +0900

    feat: add helpers for color picker

---
+++ src/components/helpers/color.js
@@ -0,0 +1,23 @@
+export const COLOR_PALETTE = {
+  black: '#2c2c2c',
+  white: '#ffffff',
+  red: '#FF3B30',
+  orange: '#ff9500',
+  yellow: '#ffcc00',
+  green: '#4cd963',
+  skyblue: '#5ac8fa',
+  deepblue: '#0579ff',
+  purple: '#5856d6',
+};
+
+export const COLOR_PALETTE_ARRAY = [
+  '#2c2c2c',
+  '#ffffff',
+  '#FF3B30',
+  '#ff9500',
+  '#ffcc00',
+  '#4cd963',
+  '#5ac8fa',
+  '#0579ff',
+  '#5856d6',
+];
