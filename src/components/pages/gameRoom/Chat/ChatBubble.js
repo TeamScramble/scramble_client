db5d14119e68662fbd2d76bcbf8d4caa8a5f5b1b
dahee kim <ekekekgml@naver.com>
2021-09-06 00:19:57 +0900

    feat: change chatbubble css style for each message type

--- src/components/pages/gameRoom/Chat/ChatBubble.js
+++ src/components/pages/gameRoom/Chat/ChatBubble.js
@@ -1,50 +1,87 @@
-import { socket } from 'context/SocketContext';
-import React from 'react';
+import React, { useContext } from 'react';
 import styled, { css } from 'styled-components';
+import { MESSAGE_TYPE } from 'components/helpers/constants';
+import { SocketContext, UserContext } from 'context';
+
+const MyMessage = css`
+  margin-left: auto;
+  .messageContainer {
+    display: flex;
+    flex-basis: content;
+    align-items: center;
+    width: auto;
+    height: auto;
+    span {
+      margin-left: auto;
+      word-break: break-word;
+      border: 1px solid #ddd;
+      padding: 10px 10px;
+      border-radius: 17px 0 17px 17px;
+    }
+  }
+`;
+
+const MySolvedMessage = css`
+  ${() => MyMessage};
+  color: gray;
+`;
+
+const OtherSolvedMessage = css`
+  ${() => OtherMessage}
+  color: gray;
+`;
+
+const OtherMessage = css`
+  .messageContainer {
+    display: flex;
+    flex-basis: content;
+    align-items: center;
+    width: auto;
+    height: auto;
+    span {
+      word-break: break-word;
+
+      border: 1px solid #ddd;
+      padding: 10px 10px;
+
+      border-radius: 0 17px 17px 17px;
+    }
+  }
+`;
+
+const CorrectMessage = css`
+  color: green;
+  border: none;
+`;
+
+const SystemMessage = css`
+  color: blue;
+  border: none;
+`;
+
+const Solved = css`
+  color: red;
+  border: none;
+`;
 
 const ChatBubbleContainer = styled.div`
   margin: 0 0 10px 0;
 
-  .messageContainer {
-    span {
-      word-break: break-word;
-      width: auto;
-      height: auto;
-      flex-basis: content;
-      align-items: center;
-      border: 1px solid #ddd;
-      padding: 10px 10px;
+  ${props => {
+    switch (props.type) {
+      case MESSAGE_TYPE.correct:
+        return CorrectMessage;
+      case MESSAGE_TYPE.system:
+        return SystemMessage;
+      case MESSAGE_TYPE.solved:
+        return props.isMine ? MySolvedMessage : OtherSolvedMessage;
+
+      case MESSAGE_TYPE.all:
+        return props.isMine ? MyMessage : OtherMessage;
+      default:
+        return;
     }
-  }
-
-  ${props =>
-    props.isMine
-      ? css`
-          margin-left: auto;
-          .messageContainer {
-            display: flex;
-
-            span {
-              margin-left: auto;
-              word-break: break-word;
-              width: auto;
-              height: auto;
-              flex-basis: content;
-              align-items: center;
-              border: 1px solid #ddd;
-              padding: 10px 10px;
-              border-radius: 17px 0 17px 17px;
-            }
-          }
-        `
-      : css`
-          .messageContainer {
-            display: flex;
-            span {
-              border-radius: 0 17px 17px 17px;
-            }
-          }
-        `}
+  }}
 
   .nickname {
     font-size: 13px;
@@ -53,14 +90,35 @@ const ChatBubbleContainer = styled.div`
 `;
 
 const ChatBubble = ({ item }) => {
-  return (
-    <ChatBubbleContainer isMine={item.user_id === socket.id}>
-      <div className="nickname">{item.nickname}</div>
-      <div className="messageContainer">
-        <span>{item.message}</span>
-      </div>
-    </ChatBubbleContainer>
-  );
+  const socket = useContext(SocketContext);
+  const { isSolved } = useContext(UserContext);
+  if (item.type === MESSAGE_TYPE.solved) {
+    return isSolved ? (
+      <ChatBubbleContainer isMine={item.user_id === socket.id} type={item.type}>
+        <div className="nickname">{item.nickname}</div>
+        <div className="messageContainer">
+          <span>{item.message}</span>
+        </div>
+      </ChatBubbleContainer>
+    ) : (
+      <div />
+    );
+  } else {
+    return (
+      <ChatBubbleContainer isMine={item.user_id === socket.id} type={item.type}>
+        {item.type === MESSAGE_TYPE.all ? (
+          <>
+            <div className="nickname">{item.nickname}</div>
+            <div className="messageContainer">
+              <span>{item.message}</span>
+            </div>
+          </>
+        ) : (
+          <span>{item.message}</span>
+        )}
+      </ChatBubbleContainer>
+    );
+  }
 };
 
 export default ChatBubble;
