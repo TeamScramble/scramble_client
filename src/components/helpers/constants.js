3506653cd4943ecaca2a274838b348eaae21b64a
dahee kim <ekekekgml@naver.com>
2021-09-05 23:59:40 +0900

    refactor: refactor dependency stucture

---
+++ src/components/helpers/constants.js
@@ -0,0 +1,13 @@
+const MESSAGE_TYPE = {
+  solved: 'SOLVED',
+  all: 'ALL',
+  correct: 'CORRECT',
+  system: 'SYSTEM',
+};
+
+const FOREGROUND_TYPE = {
+  choiceWord: 'CHOICE_WORD',
+  finishSet: 'FINISH_SET',
+};
+
+export { MESSAGE_TYPE, FOREGROUND_TYPE };
